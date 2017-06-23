dd5118dd25eae0e656464dccf7be06a895ee7bc4
Jonathan Robie <jonathan.robie@biblicalhumanities.org>
2017-06-23 18:09:49 -0400

    Now handles basic trees for XML or bracket notation. Milestones / annotations / extensions not yet supported.

--- src/treedown.py
+++ src/treedown.py
@@ -1,33 +1,13 @@
 # -*- coding: UTF-8
 #  Treedown utility
 #
-#  Converts Treedown to bracket notation or Lowfat XML, 
-#  might also support HTML for literate programming.
+#  Converts Treedown to bracket notation or Lowfat XML.
 
 import re
 import sys
 import argparse
 import codecs
-
-#  Command line:
-#
-#  treedown input.td - writes to stdout
-#  -b  emit bracket notation (default)
-#  -xml emit Lowfat XML
-
-argparser = argparse.ArgumentParser(description='Treedown utility')
-argparser.add_argument('input', metavar='infile', type=str, help='input file (in Treedown format)')
-args = argparser.parse_args()
-
-# Milestones
-# [ ] brackets
-# [ ] XML
-
-# Don't rely on blank line at end to finish!  If you have processed
-# the last line, see if you have to emit something.
-
-# Put everything that is not part of the text first in the pattern. 
-# That makes it a lot easier to "print out the rest".
+import string
 
 scanner = re.compile(r'''
   ^(\s+) |                     # left-hand whitespace
@@ -35,78 +15,177 @@ scanner = re.compile(r'''
   ([-]+) |                     # dashes - may be keywords or punctuation in text
   (\#.+)  |                    # comments
   (\w+) |                      # words
-  ([\.\;\"··]) |               # punctuation - incomplete ".,·;—>·()"
+  ([\.\;\"··\-,]) |             # punctuation - incomplete ".,·;—>·()"
   (\s+) |                      # generic whitespace
   (.)                          # an error!
 ''', re.DOTALL | re.VERBOSE | re.UNICODE)
 
 #  TODO: Consider line continuation
 
-def blank_line(tokens):
-  if tokens is None:
-    return False
-  elif tokens==[] or len(tokens)==1 and tokens[0][0] != "":
-    return True
-  else:
-    return False
+class BracketEmitter:
+    def labeled_leftbracket(self, label):
+      print "["+str(label)
 
-# Don't rely on indentation for comments / milestones / annotations
-def indent(tokens):
-  if blank_line(tokens):
-    level = 0
-  elif tokens[0][0] == "":
-    level = 1
-  else:
-    spaces = tokens[0][0].count(' ')
-    tabs = tokens[0][0].count('\t')
-    if spaces > 0:
-      if tabs > 0:
-        sys.exit("Don't mix tabs and spaces for indentation")
-      if spaces % 4 != 0:
-        sys.exit("If indenting with spaces, use 4 spaces per level.")
-      else:
-        tabs = spaces / 4
-    level = tabs + 1
-  return level 
+    def leftbracket(self):
+      print "["
 
-class Emitter:
+    def rightbracket(self):
+      print "]"
+
+    def word(self, string):
+      print string
+
+    def punc(self, string):
+      print string
+
+    def whitespace(self, string):
+      print string 
+
+    def unexpected(self, string):
+      print string
+
+class XMLEmitter:
+    def labeled_leftbracket(self, label):
+      print '<wg role="'+str(label)+'">'
+
+    def leftbracket(self):
+      print "<wg>"
+
+    def rightbracket(self):
+      print "</wg>"
+
+    def word(self, string):
+      print "<w>" + string + "</w>" 
+
+    def punc(self, string):
+      print "<pc>" + string + "</pc>"
+
+    def whitespace(self, string):
+      return 
+
+    def unexpected(self, string):
+      print "<error>" + string + "</error>" 
+
+
+class LineParser:
   level = 0      # 0 = not in sentence, 1 = top level in sentence, etc.
-
+  emitter = None
 
   def __init__(self, type):   #  type = 'brackets', 'xml', or 'normalize'
       self.type = type
+      if type == "brackets":
+        self.emitter = BracketEmitter()
+      elif type == "xml":
+        self.emitter = XMLEmitter()
 
+  def milestone(self, tokens):
+    return False
 
-  def emit(self, line):
+  def comment_line(self, tokens):
+    if len(tokens)==1 and tokens[0][3] != "":
+      return True
+    elif len(tokens)==2 and tokens[1][3] != "":
+      return True
+    else:
+      return False
+
+  def blank_line(self, tokens):
+    if tokens is None:
+      return False
+    elif tokens==[] or len(tokens)==1 and tokens[0][0] != "":
+      return True
+    else:
+      return False
+
+  def indentation(self, tokens):
+    if self.blank_line(tokens):
+      level = 0
+    elif tokens[0][0] == "":
+      level = 1
+    else:
+      spaces = tokens[0][0].count(' ')
+      tabs = tokens[0][0].count('\t')
+      if spaces > 0:
+        if tabs > 0:
+          sys.exit("Don't mix tabs and spaces for indentation")
+        if spaces % 4 != 0:
+          sys.exit("If indenting with spaces, use 4 spaces per level.")
+        else:
+          tabs = spaces / 4
+      level = tabs + 1
+    return level 
+
+  def process(self, line):
     tokens = scanner.findall(line) 
-    if blank_line(tokens):
+
+    if self.milestone(tokens):
+      return
+    if self.comment_line(tokens):
+      return
+    if self.blank_line(tokens):
       return
 
-    newlevel = indent(tokens)
+    newlevel = self.indentation(tokens)
 
     if newlevel > 0 and self.level == 0: # beginning of sentence
-        print "[S "  # 0 -> 1
+        self.emitter.labeled_leftbracket("S")
         lbracks = range(self.level + 1, newlevel)
     else:
         lbracks = range(self.level, newlevel)
     for i in lbracks:
-        print "[ " 
+        self.emitter.leftbracket()
 
     for i in range(newlevel, self.level):
-      print "]"
+      self.emitter.rightbracket()
 
-    print "["+line.strip()+"]"
+    self.line_content(tokens)
 
+    self.level = newlevel 
 
-    self.level = newlevel   
+  def line_content(self, tokens):
+    label = False
+    for t in tokens:
+      if t[0] != "":  # indentation
+        continue
+      elif t[1] != "": # label
+        self.emitter.labeled_leftbracket(t[1])
+        label = True
+      elif t[2] != "":  # dash ... TODO
+        continue
+      elif t[3] != "": # TD comment
+        continue
+      elif t[4] != "": # word
+        self.emitter.word(t[4])
+      elif t[5] != "": # punctuation
+        self.emitter.punc(t[5])
+      elif t[6] != "": # whitespace
+        self.emitter.whitespace(t[6])
+      elif t[7] != "": # unexpected
+        self.emitter.unexpected(t[7])
 
+    if label == True:
+        self.emitter.rightbracket()
 
   def cleanup(self):
     for i in range(0, self.level):
-      print "]"
+      self.emitter.rightbracket()
+
+argparser = argparse.ArgumentParser(description='Treedown utility')
+argparser.add_argument('input', metavar='infile', type=str, help='input file (in Treedown format)')
+argparser.add_argument("-b", "--brackets", help="produce bracket notation instead of XML", action="store_true")
+args = argparser.parse_args()
 
 f = codecs.open(args.input, encoding='utf-8', mode='r')
-e = Emitter('brackets')
+if args.brackets:
+  lp = LineParser('brackets')
+else:
+  lp = LineParser('xml')
 for line in f:
-    e.emit(line)
-e.cleanup()
+    lp.process(line)
+lp.cleanup() 
+
+# Full parsing of line
+# Complete XML structure
+# Add pattern for annotations
+# Add pattern for milestones
+# Don't rely on indentation for comments / milestones / annotations
