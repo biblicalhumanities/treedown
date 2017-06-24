45c5e84ebf401a9249c96cb5af0d3387192e2579
Jonathan Robie <jonathan.robie@biblicalhumanities.org>
2017-06-24 17:39:50 -0400

    Fixed some style issues detected with flake8

--- src/treedown.py
+++ src/treedown.py
@@ -7,12 +7,11 @@ import re
 import sys
 import argparse
 import codecs
-import string
 
 scanner = re.compile(r'''
     ^(\s+) |                     # left-hand whitespace
-    ([\+\:sv]|pc|od|oi) |        # keywords - needs refinement for latin char languages
-    ([-]+) |                     # dashes - may be keywords or punctuation in text
+    ([\+\:sv]|pc|od|oi) |        # keywords
+    ([-]+) |                     # dashes - keywords or punctuation
     (\#.+)  |                    # comments
     (\w+) |                      # words
     ([\.\;\"··\-,]) |            # punctuation - incomplete ".,·;—>·()"
@@ -20,7 +19,6 @@ scanner = re.compile(r'''
     (.)                          # an error!
 ''', re.DOTALL | re.VERBOSE | re.UNICODE)
 
-#  TODO: Consider line continuation
 
 class BracketEmitter:
     def labeled_leftbracket(self, label):
@@ -39,10 +37,11 @@ class BracketEmitter:
         print string
 
     def whitespace(self, string):
-        print string 
+        print string
 
     def unexpected(self, string):
         print string
+
 
 class XMLEmitter:
     def labeled_leftbracket(self, label):
@@ -55,32 +54,32 @@ class XMLEmitter:
         print "</wg>"
 
     def word(self, string):
-        print "<w>" + string + "</w>" 
+        print "<w>" + string + "</w>"
 
     def punc(self, string):
         print "<pc>" + string + "</pc>"
 
     def whitespace(self, string):
-        return 
+        return
 
     def unexpected(self, string):
-        print "<error>" + string + "</error>" 
+        print "<error>" + string + "</error>"
 
 
 class LineParser:
     level = 0      # 0 = not in sentence, 1 = top level in sentence, etc.
     emitter = None
 
-    def __init__(self, emitter):   #  type = 'brackets', 'xml', or 'normalize'
+    def __init__(self, emitter):   # type = 'brackets', 'xml', or 'normalize'
         self.emitter = emitter
 
     def milestone(self, tokens):
         return False
 
     def comment_line(self, tokens):
-        if len(tokens)==1 and tokens[0][3] != "":
+        if len(tokens) == 1 and tokens[0][3] != "":
             return True
-        elif len(tokens)==2 and tokens[1][3] != "":
+        elif len(tokens) == 2 and tokens[1][3] != "":
             return True
         else:
             return False
@@ -88,7 +87,7 @@ class LineParser:
     def blank_line(self, tokens):
         if tokens is None:
             return False
-        elif tokens==[] or len(tokens)==1 and tokens[0][0] != "":
+        elif tokens == [] or len(tokens) == 1 and tokens[0][0] != "":
             return True
         else:
             return False
@@ -109,10 +108,10 @@ class LineParser:
                 else:
                     tabs = spaces / 4
             level = tabs + 1
-        return level 
+        return level
 
     def process(self, line):
-        tokens = scanner.findall(line) 
+        tokens = scanner.findall(line)
 
         if self.milestone(tokens):
             return
@@ -123,7 +122,7 @@ class LineParser:
 
         newlevel = self.indentation(tokens)
 
-        if newlevel > 0 and self.level == 0: # beginning of sentence
+        if newlevel > 0 and self.level == 0:  # beginning of sentence
             self.emitter.labeled_leftbracket("S")
             lbracks = range(self.level + 1, newlevel)
         else:
@@ -136,35 +135,36 @@ class LineParser:
 
         self.line_content(tokens)
 
-        self.level = newlevel 
+        self.level = newlevel
 
     def line_content(self, tokens):
         label = False
         for t in tokens:
-            if t[0] != "":  # indentation
+            if t[0] != "":    # indentation
                 continue
-            elif t[1] != "": # label
+            elif t[1] != "":  # label
                 self.emitter.labeled_leftbracket(t[1])
                 label = True
             elif t[2] != "":  # dash ... TODO
                 continue
-            elif t[3] != "": # TD comment
+            elif t[3] != "":  # TD comment
                 continue
-            elif t[4] != "": # word
+            elif t[4] != "":  # word
                 self.emitter.word(t[4])
-            elif t[5] != "": # punctuation
+            elif t[5] != "":  # punctuation
                 self.emitter.punc(t[5])
-            elif t[6] != "": # whitespace
+            elif t[6] != "":  # whitespace
                 self.emitter.whitespace(t[6])
-            elif t[7] != "": # unexpected
+            elif t[7] != "":  # unexpected
                 self.emitter.unexpected(t[7])
 
-        if label == True:
+        if label is True:
             self.emitter.rightbracket()
 
     def cleanup(self):
         for i in range(0, self.level):
             self.emitter.rightbracket()
+
 
 argparser = argparse.ArgumentParser(description='Treedown utility')
 argparser.add_argument('input', metavar='infile', type=str, help='input file (in Treedown format)')
@@ -180,7 +180,7 @@ else:
     lp = LineParser(XMLEmitter())
 for line in f:
     lp.process(line)
-lp.cleanup() 
+lp.cleanup()
 
 # Full parsing of line
 # Complete XML structure
